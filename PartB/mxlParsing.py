31dc305123f21ebdd8fc24959f23d1adfde14c81
EvaArevalo <evar_3712@hotmail.com>
2019-12-21 15:16:14 +0800

    Added support for accidentals

--- PartB/mxlParsing.py
+++ PartB/mxlParsing.py
@@ -10,103 +10,130 @@ class NoteTypes(Enum):
 	thirtysecond = 32
 
 class Note:
-	'''Class representing a musical note'''
-	notetype = None
-	step = ''
-	octave = 0
-	so = None
-	tie = None
-
-	def __init__(self,notetype,step,octave,tie):
-		self.notetype = notetype
-		self.step = step
-		self.octave = octave
-		self.tie = tie
-		self.so = step + str(octave)
-
-	def printNote(self):
-		print('Note: ' + str(self.so) + ' / ' +
-		str(self.notetype) + ' / ' +
-		str(self.tie))
-
+    '''Class representing a musical note'''
+    notetype = None
+    step = ''
+    octave = 0
+    so = None
+    tie = None
+    accidental = None
+    
+    def __init__(self,notetype,step,octave,tie,accidental):
+        self.notetype = notetype
+        self.step = step
+        self.octave = octave
+        self.tie = tie
+        if accidental:
+            self.so = step + accidental+ str(octave)
+        else:
+            self.so = step + str(octave)
+        self.accidental = accidental
+    
+    def printNote(self):
+        if self.step=='':
+            print('Note: ' + 'rest' + ' / ' +
+              str(self.notetype))
+        else:
+            print('Note: ' + str(self.so) + ' / ' +
+              str(self.notetype) + ' / ' +
+              str(self.tie))
+            
 class Measure:
-	'''Class representing a measure'''
-	notes = []
-	
-	def __init__(self,notes):
-		self.notes = notes
-	
-	def get_sos(self):
-		sos = []
-		for note in self.notes:
-			sos.append(note.so)
-		return sos
-	
-	def get_notetypes(self):
-		notetypes = []
-		for note in self.notes:
-			notetypes.append(note.notetype)
-		return notetypes 
-	
-	def printMeasure(self):
-		for note in self.notes:
-			note.printNote()
+    '''Class representing a measure'''
+    notes = []
+    
+    def __init__(self,notes):
+        self.notes = notes
+    
+    def get_sos(self):
+        sos = []
+        for note in self.notes:
+            sos.append(note.so)
+        return sos
+    
+    def get_notetypes(self):
+        notetypes = []
+        for note in self.notes:
+            notetypes.append(note.notetype)
+        return notetypes 
+    
+    def get_ties(self):
+        ties = []
+        for tie in self.notes:
+            ties.append(note.tie)
+        return ties 
+    
+    def printMeasure(self):
+        for note in self.notes:
+            note.printNote()
 
 
 def get_ts_xml(root):
-	'''Gets time signature for a musicxml file.
-	Input: Root of XML Tree
-	Output:top and bottom numbers of the Time signature'''
-	time = root.find('.//time')
-	top = int(time.find(".//beats").text)
-	bottom = int(time.find(".//beat-type").text)
-	return top,bottom
+    '''Gets time signature for a musicxml file.
+    Input: Root of XML Tree
+    Output:top and bottom numbers of the Time signature'''
+    time = root.find('.//time')
+    top = int(time.find(".//beats").text)
+    bottom = int(time.find(".//beat-type").text)
+    return top,bottom
 
 def get_bpm_xml(root):
-	'''Gets beats per minute as well as type of beats for a musicxml file.
-	Input: Root of XML Tree
-	Output: beats per minute, noteType'''
-	metronome = root.find('.//metronome')
-	beats_per_min = int(metronome.find('.//per-minute').text)
-	beat_type = metronome.find('.//beat-unit').text
-	#beat_type = getattr(NoteTypes, str(beat_type))
-	return beats_per_min, beat_type
+    '''Gets beats per minute as well as type of beats for a musicxml file.
+    Input: Root of XML Tree
+    Output: beats per minute, noteType'''
+    metronome = root.find('.//metronome')
+    beats_per_min = int(metronome.find('.//per-minute').text)
+    beat_type = metronome.find('.//beat-unit').text
+    #beat_type = getattr(NoteTypes, str(beat_type))
+    return beats_per_min, beat_type
 
 def decode_xml_note(xml_note):
-	'''Gets note type, step, octave and tie type'''
-	try:
-		notetype = xml_note.find('.//type').text
-		if notetype == '16th':
-			notetype = 'sixteenth'
-		elif notetype == '32nd':
-			notetype = 'thirtysecond'
-	except:
-		notetype = None
-	try:
-		step = xml_note.find('.//step').text
-	except:
-		step = ''
-	try:
-		octave = xml_note.find('.//octave').text
-	except:
-		octave = 0
-	try:
-		tie = xml_note.find('.//tie').text
-	except:
-		tie = None
-		
-	note = Note(notetype,step,octave,tie)
-	#DEBUG
-	#note.printNote()
-	
-	return note
+    '''Gets note type, step, octave and tie type'''
+    try:
+        notetype = xml_note.find('.//type').text
+        if notetype == '16th':
+            notetype = 'sixteenth'
+        elif notetype == '32nd':
+            notetype = 'thirtysecond'
+    except:
+        notetype = None
+    try:
+        step = xml_note.find('.//step').text
+    except:
+        step = ''
+    try:
+        octave = xml_note.find('.//octave').text
+    except:
+        octave = 0
+    try:
+        tie = xml_note.find('.//tie').get('type')
+    except:
+        tie = None
+    try:
+        accidental = xml_note.find('.//accidental').text
+        if accidental == 'natural':
+            accidental = '*'
+        if accidental == 'flat':
+            accidental = 'b'
+        if accidental == 'sharp':
+            accidental = '#'
+            
+    except:
+        accidental = None
+        
+    note = Note(notetype,step,octave,tie,accidental)
+    #DEBUG
+    #note.printNote()
+    
+    return note
 
 # return all notes in a measure
 def get_meas_notes_xml(xml_measure_notes):
-	notes = []
-	for xml_note in xml_measure_notes:
-		note = decode_xml_note(xml_note)
-		notes.append(note)
-		#DEBUG
-		#note.printNote()
-	return notes
+    notes = []
+    for xml_note in xml_measure_notes:
+        note = decode_xml_note(xml_note)
+        if note.notetype:
+            notes.append(note)
+        #DEBUG
+        #note.printNote()
+    return notes
